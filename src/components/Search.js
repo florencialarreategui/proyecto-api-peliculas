0e943eb010483b370122d355ed2d1dcf1f70b80c
florencialarreategui <florencialarreategui@gmail.com>
2022-03-31 12:41:11 -0300

    agrego botó para prueba

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Container from "@mui/material/Container";
+import Tarjetas from "./Tarjetas";
+import Button from '@mui/material/Button';
 
 const Search = () => {
   const [valorDelInput, setValorDelInput] = useState("");
@@ -10,28 +12,27 @@ const Search = () => {
   const [searchParams, setSearchParams] = useSearchParams({
     query: "",
   });
-  const api_key = a956e692d7f996ce28c6cca9dfa978ce;
 
   useEffect(() => {
     fetch(
-      `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${searchParams.get(
-        "query"
-      )}`
+      `https://api.themoviedb.org/3/search/movie?api_key=a956e692d7f996ce28c6cca9dfa978ce&query=${searchParams.get("query")}`
     )
-      .then((res) => res.jason())
-      .then((data) => {
+      .then(res => res.json())
+      .then(data => {
         setPeliculas(data.results);
       });
   }, [searchParams]);
 
   const handleChange = (e) => {
     setValorDelInput(e.target.value);
+    console.log("valor del input");
   };
 
-  const handlePress = () => {
+  const handleClick = () => {
     setSearchParams({
       query: valorDelInput,
     });
+    console.log("presionaste la tecla");
   };
   return (
     <Container
@@ -49,9 +50,13 @@ const Search = () => {
         variant="outlined"
         onChange={handleChange}
         value={valorDelInput}
-        onKeyPress={handlePress}
+
         sx={{ mt: 10, width: 600 }}
       />
+      <Button size="small" color="primary" onClick={handleClick}>
+        Buscar
+      </Button>
+
     </Container>
   );
 };
