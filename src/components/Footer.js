cfb168186fb3d7e8ae08dcf0d8723f7170a662a2
florencialarreategui <florencialarreategui@gmail.com>
2022-04-22 23:20:41 -0300

    arreglo footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,38 @@
 import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { FaGithubAlt } from "react-icons/fa";
+import { BsLinkedin } from "react-icons/bs";
 const Footer = () => {
   return (
-    <>
-      <Box sx={{
+    <Box
+      sx={{
         height: 100,
         backgroundColor: "warning.dark",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
       }}
-      >
+    >
+      <Box sx={{ justifyContent: "center", color: "#fafafa" }}>
+        <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
+          <Box>Hecho por Flor</Box>
+        </Typography>
+        <Box sx={{ flexDirection: "column", justifyContent: "center" }}>
+          <Box>
+            <FaGithubAlt />{" "}
+            <a href="https://github.com/florencialarreategui">Github</a>
+          </Box>
 
+          <Box>
+            <BsLinkedin />{" "}
+            <a href="https://www.linkedin.com/in/florencia-larreategui/">
+              Linkedin
+            </a>
+          </Box>
+        </Box>
       </Box>
-    </>
+    </Box>
+
   );
 };
 
